7f4e0ecc409d0c1ca238470e4ac106a2a58410ec
Julian von Gebhardi <julian@happyinbox.io>
2023-02-19 20:55:48 +0100

    goback

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,12 @@
 import { defineConfig } from 'astro/config';
 import storyblok from '@storyblok/astro';
 import { loadEnv } from 'vite';
-import netlify from '@astrojs/netlify/functions';
 
 import svelte from '@astrojs/svelte';
 
 const env = loadEnv('', process.cwd(), 'STORYBLOK');
 // https://astro.build/config
 export default defineConfig({
-  adapter: netlify(),
   integrations: [
     svelte(),
     storyblok({
