5fadb97b44d85e20b7227b77f9ad5842cefd5850
Julian von Gebhardi <julian@happyinbox.io>
2023-02-19 18:05:59 +0100

    example

--- src/components/sections/Gallery.svelte
+++ src/components/sections/Gallery.svelte
@@ -3,76 +3,94 @@
 
 <div class="columns is-multiline is-mobile m-0 p-0">
   <div class="column is-4 p-0">
-    <img
-      class="image is-fullwidth"
-      style="height: 307px; object-fit: cover;"
-      src="https://images.unsplash.com/photo-1536851101967-55988a52f455?ixlib=rb-1.2.1&amp;ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=1050&amp;q=80"
-      alt=""
-    />
+    <a href="/product"
+      ><img
+        class="image is-fullwidth"
+        style="height: 307px; object-fit: cover;"
+        src="https://images.unsplash.com/photo-1536851101967-55988a52f455?ixlib=rb-1.2.1&amp;ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=1050&amp;q=80"
+        alt=""
+      /></a
+    >
   </div>
   <div class="column is-4 p-0">
-    <img
-      class="image is-fullwidth"
-      style="height: 307px; object-fit: cover;"
-      src="https://images.unsplash.com/photo-1553949345-eb786bb3f7ba?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1050&amp;q=80"
-      alt=""
-    />
+    <a href="/product"
+      ><img
+        class="image is-fullwidth"
+        style="height: 307px; object-fit: cover;"
+        src="https://images.unsplash.com/photo-1553949345-eb786bb3f7ba?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1050&amp;q=80"
+        alt=""
+      /></a
+    >
   </div>
   <div class="column is-4 p-0">
-    <img
-      class="image is-fullwidth"
-      style="height: 307px; object-fit: cover;"
-      src="https://images.unsplash.com/photo-1552844657-1d8b11fad02e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1003&amp;q=80"
-      alt=""
-    />
+    <a href="/product"
+      ><img
+        class="image is-fullwidth"
+        style="height: 307px; object-fit: cover;"
+        src="https://images.unsplash.com/photo-1552844657-1d8b11fad02e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1003&amp;q=80"
+        alt=""
+      /></a
+    >
   </div>
   <div class="column is-4 p-0">
-    <img
-      class="image is-fullwidth"
-      style="height: 307px; object-fit: cover;"
-      src="https://images.unsplash.com/photo-1593776805861-74972b600999?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1050&amp;q=80"
-      alt=""
-    />
+    <a href="/product"
+      ><img
+        class="image is-fullwidth"
+        style="height: 307px; object-fit: cover;"
+        src="https://images.unsplash.com/photo-1593776805861-74972b600999?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1050&amp;q=80"
+        alt=""
+      /></a
+    >
   </div>
   <div class="column is-4 p-0">
-    <img
-      class="image is-fullwidth"
-      style="height: 307px; object-fit: cover;"
-      src="https://images.unsplash.com/photo-1552844657-1d8b11fad02e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1003&amp;q=80"
-      alt=""
-    />
+    <a href="/product"
+      ><img
+        class="image is-fullwidth"
+        style="height: 307px; object-fit: cover;"
+        src="https://images.unsplash.com/photo-1552844657-1d8b11fad02e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1003&amp;q=80"
+        alt=""
+      /></a
+    >
   </div>
   <div class="column is-4 p-0">
-    <img
-      class="image is-fullwidth"
-      style="height: 307px; object-fit: cover;"
-      src="https://images.unsplash.com/photo-1536851101967-55988a52f455?ixlib=rb-1.2.1&amp;ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=1050&amp;q=80"
-      alt=""
-    />
+    <a href="/product"
+      ><img
+        class="image is-fullwidth"
+        style="height: 307px; object-fit: cover;"
+        src="https://images.unsplash.com/photo-1536851101967-55988a52f455?ixlib=rb-1.2.1&amp;ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=1050&amp;q=80"
+        alt=""
+      /></a
+    >
   </div>
   <div class="column is-4 p-0">
-    <img
-      class="image is-fullwidth"
-      style="height: 307px; object-fit: cover;"
-      src="https://images.unsplash.com/photo-1553949345-eb786bb3f7ba?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1050&amp;q=80"
-      alt=""
-    />
+    <a href="/product"
+      ><img
+        class="image is-fullwidth"
+        style="height: 307px; object-fit: cover;"
+        src="https://images.unsplash.com/photo-1553949345-eb786bb3f7ba?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1050&amp;q=80"
+        alt=""
+      /></a
+    >
   </div>
   <div class="column is-4 p-0">
-    <img
-      class="image is-fullwidth"
-      style="height: 307px; object-fit: cover;"
-      src="https://images.unsplash.com/photo-1553949345-eb786bb3f7ba?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1050&amp;q=80"
-      alt=""
-    />
+    <a href="/product"
+      ><img
+        class="image is-fullwidth"
+        style="height: 307px; object-fit: cover;"
+        src="https://images.unsplash.com/photo-1553949345-eb786bb3f7ba?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1050&amp;q=80"
+        alt=""
+      /></a
+    >
   </div>
   <div class="column is-4 p-0">
-    <img
-      class="image is-fullwidth"
-      style="height: 307px; object-fit: cover;"
-      src="https://images.unsplash.com/photo-1553949345-eb786bb3f7ba?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1050&amp;q=80"
-      alt=""
-    />
+    <a href="/product"
+      ><img
+        class="image is-fullwidth"
+        style="height: 307px; object-fit: cover;"
+        src="https://images.unsplash.com/photo-1553949345-eb786bb3f7ba?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;ixlib=rb-1.2.1&amp;auto=format&amp;fit=crop&amp;w=1050&amp;q=80"
+        alt=""
+      /></a
+    >
   </div>
 </div>
 
