8b37f536f1001aae88f6bf797ce514f77add72b3
Julian von Gebhardi <julian@happyinbox.io>
2023-02-19 17:55:05 +0100

    intial page

---
+++ src/components/sections/Navigation.svelte
@@ -0,0 +1,135 @@
+<div class="is-relative has-background-primary">
+  <nav class="is-relative navbar py-2 has-background-transparent">
+    <div class="navbar-brand is-align-items-center is-flex-grow-1">
+      <a class="navbar-item" href="#">
+        <svg
+          width="22"
+          height="23"
+          viewBox="0 0 22 23"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M21.7973 20.0328L16.4361 14.4697C18.7112 10.9316 18.2607 6.2297 15.3549 3.22699C13.7105 1.55107 11.548 0.619995 9.22781 0.619995C6.90764 0.619995 4.74515 1.55107 3.12329 3.22699C-0.233072 6.71851 -0.233072 12.3748 3.12329 15.843C6.02913 18.8457 10.5794 19.3112 14.0033 16.9836L19.387 22.5467C19.6348 22.8028 20.0628 22.8028 20.3106 22.5467L21.7973 21.0104C22.0676 20.7311 22.0676 20.2889 21.7973 20.0328ZM13.3726 13.8179C11.0975 16.1689 7.38069 16.1689 5.08304 13.8179C2.80793 11.467 2.80793 7.6263 5.08304 5.25207C7.35816 2.90112 11.0749 2.90112 13.3726 5.25207C15.6477 7.6263 15.6477 11.467 13.3726 13.8179Z"
+            fill="white"
+          />
+        </svg>
+      </a>
+      <a
+        class="navbar-menu-open navbar-burger is-hidden-desktop is-flex is-align-items-center is-justify-content-center"
+        role="button"
+        type="button"
+        data-toggle="side-menu"
+      >
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+          ><rect x="2" y="6" width="20" height="2" fill="white" /><rect
+            x="2"
+            y="11"
+            width="20"
+            height="2"
+            fill="white"
+          /><rect x="2" y="16" width="20" height="2" fill="white" /></svg
+        >
+      </a>
+    </div>
+    <div
+      class="is-absolute"
+      style="top: 50%; left: 50%; transform: translateY(-50%) translateX(-50%);"
+    >
+      <a class="navbar-item" href="#">
+        <span class="navbar-item px-0 title is-size-5 has-text-dark">
+          <img
+            src="/bendis-assets/logos/bendis-light.svg"
+            alt=""
+            style="max-height: initial;"
+          />
+        </span>
+      </a>
+    </div>
+    <div class="navbar-menu">
+      <div class="navbar-end">
+        <a
+          class="navbar-menu-open navbar-burger is-flex is-align-items-center is-justify-content-center"
+          role="button"
+          type="button"
+          data-toggle="side-menu"
+        >
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            ><rect x="2" y="6" width="20" height="2" fill="white" /><rect
+              x="2"
+              y="11"
+              width="20"
+              height="2"
+              fill="white"
+            /><rect x="2" y="16" width="20" height="2" fill="white" /></svg
+          >
+        </a>
+      </div>
+    </div>
+  </nav>
+  <div
+    class="is-hidden navbar-side is-fixed is-top-0 is-bottom-0 is-left-0 has-mw-md"
+    style="width: 75%; z-index: 9999;"
+  >
+    <div
+      class="navbar-backdrop is-fixed is-inset-0 has-background-dark"
+      style="opacity: 75%; z-index: 50;"
+    />
+    <aside
+      class="menu is-relative is-flex is-flex-direction-column py-8 px-10 has-background-white"
+      style="width: 100%; height: 100%; z-index: 50; overflow-y: auto;"
+    >
+      <a class="mb-20" href="#">
+        <span class="navbar-item px-0 title is-5 has-text-dark">
+          <img
+            src="/bendis-assets/logos/bendis-blue.svg"
+            style="max-height: initial"
+            alt=""
+          />
+        </span>
+      </a>
+      <ul class="mb-40">
+        <li>
+          <a class="button is-ghost has-text-left has-text-primary" href="#"
+            >About</a
+          >
+        </li>
+        <li>
+          <a class="button is-ghost has-text-left has-text-primary" href="#"
+            >Company</a
+          >
+        </li>
+        <li>
+          <a class="button is-ghost has-text-left has-text-primary" href="#"
+            >Services</a
+          >
+        </li>
+        <li>
+          <a class="button is-ghost has-text-left has-text-primary" href="#"
+            >Testimonials</a
+          >
+        </li>
+      </ul>
+      <div class="mb-8">
+        <a class="button is-ghost has-text-primary is-fullwidth" href="#"
+          >Sign In</a
+        ><a class="button is-outlined is-primary is-fullwidth" href="#"
+          >Sign up</a
+        >
+      </div>
+      <small class="has-text-centered has-text-primary"
+        >All rights reserved © Wireframes Corporation 2022</small
+      >
+    </aside>
+  </div>
+</div>
---
+++ src/components/sections/Product.svelte
@@ -0,0 +1,104 @@
+<section class="is-relative section pt-24 py-40-desktop">
+  <img
+    class="image is-hidden-mobile is-hidden-tablet-only is-absolute is-top-0 is-left-0 is-fullheight has-mw-md has-mw-xl-desktop has-mw-3xl-widescreen clip-path-right-top"
+    style="object-fit: cover;"
+    src="https://images.unsplash.com/photo-1536851101967-55988a52f455?ixlib=rb-1.2.1&amp;ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=1036&amp;q=80"
+    alt=""
+  /><img
+    class="image is-hidden-desktop mb-16"
+    style="object-fit: cover;"
+    src="https://images.unsplash.com/photo-1536851101967-55988a52f455?ixlib=rb-1.2.1&amp;ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&amp;auto=format&amp;fit=crop&amp;w=1036&amp;q=80"
+    alt=""
+  />
+  <div class="container">
+    <div class="has-mw-md mx-auto mr-0-desktop">
+      <h1 class="title is-size-3 has-leading-3 has-text-primary mb-8">
+        Lorem ipsum
+      </h1>
+      <div class="is-flex mb-8">
+        <div class="is-flex-shrink-0">
+          <svg
+            width="26"
+            height="21"
+            viewBox="0 0 26 21"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            ><path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M8.8075 15.5254L23.3675 0.965393L26 3.59789L8.8075 20.8229L0 12.0479L2.665 9.38289L8.8075 15.5254Z"
+              fill="#3C74F9"
+            /></svg
+          >
+        </div>
+        <div class="ml-4">
+          <h3
+            class="is-size-5 has-leading-4 has-text-primary has-text-weight-semibold mb-2"
+          >
+            Quick problem-solving contact
+          </h3>
+          <p class="has-text-grey has-leading-6">
+            Etiam pellentesque non nibh non pulvinar. Mauris posuere, tellus sit
+            amet tempus vestibulum.
+          </p>
+        </div>
+      </div>
+      <div class="is-flex mb-8">
+        <div class="is-flex-shrink-0">
+          <svg
+            width="26"
+            height="21"
+            viewBox="0 0 26 21"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            ><path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M8.8075 15.5254L23.3675 0.965393L26 3.59789L8.8075 20.8229L0 12.0479L2.665 9.38289L8.8075 15.5254Z"
+              fill="#3C74F9"
+            /></svg
+          >
+        </div>
+        <div class="ml-4">
+          <h3
+            class="is-size-5 has-leading-4 has-text-primary has-text-weight-semibold mb-2"
+          >
+            Quick problem-solving contact
+          </h3>
+          <p class="has-text-grey has-leading-6">
+            Etiam pellentesque non nibh non pulvinar. Mauris posuere, tellus sit
+            amet tempus vestibulum.
+          </p>
+        </div>
+      </div>
+      <div class="is-flex">
+        <div class="is-flex-shrink-0">
+          <svg
+            width="26"
+            height="21"
+            viewBox="0 0 26 21"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            ><path
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+              d="M8.8075 15.5254L23.3675 0.965393L26 3.59789L8.8075 20.8229L0 12.0479L2.665 9.38289L8.8075 15.5254Z"
+              fill="#3C74F9"
+            /></svg
+          >
+        </div>
+        <div class="ml-4">
+          <h3
+            class="is-size-5 has-leading-4 has-text-primary has-text-weight-semibold mb-2"
+          >
+            Quick problem-solving contact
+          </h3>
+          <p class="has-text-grey has-leading-6">
+            Etiam pellentesque non nibh non pulvinar. Mauris posuere, tellus sit
+            amet tempus vestibulum.
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
