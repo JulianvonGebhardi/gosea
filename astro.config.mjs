b1b635025b5f83b59a8ecfbed7690c4614e3c49b
Julian von Gebhardi <julian@happyinbox.io>
2023-02-19 21:16:41 +0100

    visuel editor

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,18 @@ import { defineConfig } from 'astro/config';
 import storyblok from '@storyblok/astro';
 import { loadEnv } from 'vite';
 import netlify from '@astrojs/netlify/functions';
-
 import svelte from '@astrojs/svelte';
 
 const env = loadEnv('', process.cwd(), 'STORYBLOK');
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   adapter: netlify(),
   integrations: [
     svelte(),
     storyblok({
       accessToken: env.STORYBLOK_TOKEN,
+      bridge: true,
       components: {
         food: 'food',
       },
